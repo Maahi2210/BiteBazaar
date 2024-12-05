8e3a11a1c7aca9a905c3c13f07793af6135daf61
Maahi2210 <shahmahi1011@gmail.com>
2024-12-05 00:22:55 -0500

    Cart Adapter,cart item,add to cart and manage the cart updated

--- app/src/main/java/com/mahi/bitebazaar/CartItem.java
+++ app/src/main/java/com/mahi/bitebazaar/CartItem.java
@@ -1,4 +1,59 @@
 package com.mahi.bitebazaar;
 
 public class CartItem {
+    private String foodId;
+    private String foodName;
+    private String foodPrice;
+    private String foodImageUrl;
+    private int quantity;
+
+    public CartItem() {
+    }
+
+    public CartItem(String foodId, String foodName, String foodPrice, int quantity) {
+        this.foodId = foodId;
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+        this.quantity = quantity;
+    }
+
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setFoodPrice(String foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public String getFoodImageUrl() {
+        return foodImageUrl;
+    }
+
+    public void setFoodImageUrl(String foodImageUrl) {
+        this.foodImageUrl = foodImageUrl;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- app/src/main/java/com/mahi/bitebazaar/CartManager.java
+++ app/src/main/java/com/mahi/bitebazaar/CartManager.java
@@ -1,4 +1,26 @@
 package com.mahi.bitebazaar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartManager {
+    private static final List<CartItem> cartItems = new ArrayList<>();
+
+    public static void addToCart(CartItem item) {
+        for (CartItem cartItem : cartItems) {
+            if (cartItem.getFoodId().equals(item.getFoodId())) {
+                cartItem.setQuantity(cartItem.getQuantity() + item.getQuantity());
+                return;
+            }
+        }
+        cartItems.add(item);
+    }
+
+    public static List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public static void clearCart() {
+        cartItems.clear();
+    }
 }
